b2052cf2cc67f32aee1e0bc357e17d7b8b2018e4
MariusS16 <slincumarius0716@gmail.com>
2025-07-14 09:51:38 +0300

    User + userManager Class v2.0

--- TaskManager/src/userManager.kt
+++ TaskManager/src/userManager.kt
@@ -1,3 +1,9 @@
 class userManager {
     var users: MutableList<User> = mutableListOf()
+
+    fun addUser (user: User) {
+        users.add(user)
+    }
+
+    fun getList () : List<User> = users.toList()
 }
